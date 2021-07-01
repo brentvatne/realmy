14141c6ccaeb6807ae5185a5fd29ad29a4584d8b
Brent Vatne <brentvatne@gmail.com>
2021-06-30 21:06:29 -0700

    ok

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-get-random-values";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import Realm from "realm";
+const Cat = {
+  name: "Cat",
+  properties: {
+    _id: "objectId",
+    name: "string",
+    age: "int",
+    type: "string",
+  },
+};
 
 export default function App() {
+  const [realm, setRealm] = React.useState();
+  const [cat, setCat] = React.useState();
+
+  React.useEffect(() => {
+    async function getRealmAsync() {
+      const db = await Realm.open({
+        schema: [Cat],
+      });
+      setRealm(db);
+    }
+
+    getRealmAsync();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>cat: {JSON.stringify(cat)}</Text>
+      <Button
+        title="Warn me"
+        onPress={() => {
+          console.warn("This is a warning!");
+        }}
+      />
+
+      <Button
+        title="Write cat"
+        onPress={() => {
+          realm.write(() => {
+            aliceCat = realm.create("Cat", {
+              _id: new Realm.BSON.ObjectID(),
+              name: "Alice",
+              age: 14,
+              type: "Calico",
+            });
+          });
+        }}
+      />
+
+      <Button
+        title="Get cat"
+        onPress={() => {
+          realm.read(() => {
+            const alice = realm
+              .objects("Cat")
+              .find((cat) => cat.name === "Alice");
+            setCat(alice);
+          });
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +72,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
